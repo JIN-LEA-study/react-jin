9b6897c144ceba546e27c0ae7a583f32e63f5525
greencoffee1635 <166354@naver.com>
2022-06-10 22:14:05 +0900

    feat: router 설정 및 Home, List 페이지 생성

--- handbook/src/App.js
+++ handbook/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import Intro from "./Intro";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import List from "./pages/List";
 
 function App() {
   return (
     <div className='App'>
-      <Intro />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/list' element={<List />} />
+      </Routes>
     </div>
   );
 }
