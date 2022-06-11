a6bc576e158d01edfefbaab4129d190e545422e6
greencoffee1635 <166354@naver.com>
2022-06-11 23:04:09 +0900

    feat: Cards, New, Practice 페이지 추가

--- handbook/src/App.js
+++ handbook/src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import List from "./pages/List";
+import Template from "./components/Template";
+import Navbar from "./components/Navbar";
+import Cards from "./pages/Cards";
+import New from "./pages/New";
+import Practice from "./pages/Practice";
+// import List from "./pages/List";
 
 function App() {
   return (
     <div className='App'>
-      <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/list' element={<List />} />
-      </Routes>
+      <div>
+        <Navbar />
+        <Template>
+          <Routes>
+            <Route path='/card' element={<Cards />} />
+            <Route path='/new' element={<New />} />
+            <Route path='/practice' element={<Practice />} />
+            {/* <Route path='/list' element={<List />} /> */}
+          </Routes>
+        </Template>
+      </div>
     </div>
   );
 }
---
+++ handbook/src/pages/New.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const New = () => {
+  return (
+    <div>
+      <h1>NEW</h1>
+    </div>
+  );
+};
+
+export default New;
