036c9acb8ff919cd4e0e551085ab604b26c8ab31
greencoffee1635 <166354@naver.com>
2022-06-11 23:06:38 +0900

    remove: List.js 삭제

--- handbook/src/App.js
+++ handbook/src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./components/Navbar";
 import Cards from "./pages/Cards";
 import New from "./pages/New";
 import Practice from "./pages/Practice";
-// import List from "./pages/List";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
             <Route path='/card' element={<Cards />} />
             <Route path='/new' element={<New />} />
             <Route path='/practice' element={<Practice />} />
-            {/* <Route path='/list' element={<List />} /> */}
           </Routes>
         </Template>
       </div>
