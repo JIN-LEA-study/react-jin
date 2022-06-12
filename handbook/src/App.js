1c29aa681ef3499704dfad99b04bfaad0a3e10fc
greencoffee1635 <166354@naver.com>
2022-06-12 22:56:42 +0900

    feat: RecoilRoot 추가

--- handbook/src/App.js
+++ handbook/src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Template from "./components/Template";
+
+// recoil
+import { RecoilRoot } from "recoil";
+
+// components
 import Navbar from "./components/Navbar";
+import Template from "./components/Template";
 import Cards from "./pages/Cards";
 import New from "./pages/New";
 import Practice from "./pages/Practice";
 
 function App() {
   return (
-    <div className='App'>
-      <div>
-        <Navbar />
-        <Template>
-          <Routes>
-            <Route path='/card' element={<Cards />} />
-            <Route path='/new' element={<New />} />
-            <Route path='/practice' element={<Practice />} />
-          </Routes>
-        </Template>
-      </div>
-    </div>
+    <RecoilRoot>
+      <Navbar />
+      <Template>
+        <Routes>
+          <Route path='/card' element={<Cards />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/practice' element={<Practice />} />
+        </Routes>
+      </Template>
+    </RecoilRoot>
   );
 }
 
