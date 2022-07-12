6594fc29375df710aa8978ca8d0e318ab79fc314
greencoffee1635 <166354@naver.com>
2022-07-12 20:11:06 +0900

    feat: 서버 연결

--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ posts }) {
 
 export const getServerSideProps = async () => {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_start=0&_end=10` // 데이터를 10개로 줄여서 가져왔다.
+    `http://localhost:8080/api/posts` // 데이터를 10개로 줄여서 가져왔다.
   );
   const posts = await res.json();
 
