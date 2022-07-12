bf0feed63e1e245844ac294cf8fbfca25a7f315d
greencoffee1635 <166354@naver.com>
2022-07-12 20:09:08 +0900

    feat: data.js 생성 및 데이터 입력

--- next-blog-local-api/app.js
+++ next-blog-local-api/app.js
@@ -3,9 +3,11 @@ const cors = require("cors");
 const app = express();
 const PORT = 8080;
 
+const { posts } = require("./data.js");
+
 app.use(cors());
-app.get("/api/post", (req, res) => {
-  res.json("");
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
 });
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
