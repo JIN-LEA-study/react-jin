0fefb4a0c7d425dcfcea700e18874f76a63b56fa
greencoffee1635 <166354@naver.com>
2022-07-12 20:48:29 +0900

    feat: getStaticProps 테스트

--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -22,22 +22,9 @@ export default function Home({ posts }) {
 
 // 하지만 NEXT.JS에서는 Static Side Generation을 추천한다.
 
-export const getServerSideProps = async () => {
-  const res = await fetch(
-    `http://localhost:8080/api/posts` // 데이터를 10개로 줄여서 가져왔다.
-  );
-  const posts = await res.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
-
-// export const getStaticProps = async () => {
+// export const getServerSideProps = async () => {
 //   const res = await fetch(
-//     `https://jsonplaceholder.typicode.com/posts?_start=0&_end=10` // 데이터를 10개로 줄여서 가져왔다.
+//     `http://localhost:8080/api/posts` // 데이터를 10개로 줄여서 가져왔다.
 //   );
 //   const posts = await res.json();
 
@@ -47,3 +34,27 @@ export const getServerSideProps = async () => {
 //     },
 //   };
 // };
+
+// 서버 데이터에 변화를 많이 주었지만, 새로고침 해도 변화가 없다.
+// Static Side Generation을 사용했기 때문이다.
+// 미리 파일을 생성, 만들어진 html을 보여주는 방식
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `http://localhost:8080/api/posts` // 데이터를 10개로 줄여서 가져왔다.
+  );
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 20, // 20초
+    // 20초 지난 시점부터 (언제든) 접속이 일어나면 새롭게 데이터를 받아서
+    // regeneration 시킬 수 있도록 파일을 새롭게 생성할 수 있도록 명령을 주는 것
+  };
+};
+
+// build를 할 때마다 페이지가 새롭게 갱신된다.
+// build 타임과 데이터가 얼마나 어떤식으로 보여져야하는지를 고려해서
+// ServerSideProps와 StaticProps를 번갈아가며 사용하면 된다.
