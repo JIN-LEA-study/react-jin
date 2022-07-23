8a711cefc3f42f95aef5c91c6b12878eaf63defd
greencoffee1635 <166354@naver.com>
2022-07-23 13:58:56 +0900

    feat: add electron notification

--- electron-chat/main.js
+++ electron-chat/main.js
@@ -1,15 +1,37 @@
-const { app, BrowserWindow } = require("electron");
+// Main Process
+const { app, BrowserWindow, Notification } = require("electron");
 
 function createWindow() {
+  // Browser Window <- Renderer Process
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
+    backgroundColor: "white",
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
   win.loadFile("index.html");
+  win.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow();
+  const notification = new Notification({
+    title: "Hello World",
+    body: "My test message",
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
