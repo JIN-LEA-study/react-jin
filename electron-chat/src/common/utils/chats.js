3cadfae0d04520cde6ffe6b4cbb0196ba8501542
greencoffee1635 <166354@naver.com>
2022-07-24 03:24:35 +0900

    fix: remove comments

--- electron-chat/src/common/utils/chats.js
+++ electron-chat/src/common/utils/chats.js
@@ -1,17 +1,9 @@
 import db from "./firestore";
 
-const extractSnapshotData = snapshot =>
+const extractSnapshotData = snapshot => {
   snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
 
-export const fetchChats = () =>
-  db
-    .collection("chats")
-    .get()
-    // .then(snapshot => {
-    //   // If you are fetching collection then data are provided under snapshot.docs
-    //   const data = snapshot.docs.map(doc => {
-    //     return doc.data();
-    //   });
-    //   return data;
-    // });
-    .then(extractSnapshotData);
+export const fetchChats = () => {
+  db.collection("chats").get().then(extractSnapshotData);
+};
