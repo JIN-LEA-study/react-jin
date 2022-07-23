202f83d17780b718de3e90441a5a972e1ed50da1
greencoffee1635 <166354@naver.com>
2022-07-23 23:16:27 +0900

    feat: add back button and more views

--- electron-chat/src/App.js
+++ electron-chat/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-import HomeView from "./views/Home";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import Settings from "./views/Settings";
+
 import Navbar from "./components/Navbar";
 
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
@@ -14,23 +18,11 @@ function App() {
           <Route path='/settings' element={<Settings />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
-          <Route path='/' element={<HomeView />} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </div>
     </Router>
   );
-}
-
-function Settings() {
-  return <h1>Settings View</h1>;
-}
-
-function Login() {
-  return <h1>Login View</h1>;
-}
-
-function Register() {
-  return <h1>Register View</h1>;
 }
 
 export default App;
--- electron-chat/src/components/Navbar.js
+++ electron-chat/src/components/Navbar.js
@@ -1,24 +1,38 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='chat-navbar'>
       <nav className='chat-navbar-inner'>
         <div className='chat-navbar-inner-left'>
+          <button
+            className='btn btn-outline-primary'
+            onClick={() => navigate(-1)}
+          >
+            Back
+          </button>
           <Link to='/settings' className='btn btn-outline-success ml-2'>
             Settings
           </Link>
         </div>
         <div className='chat-navbar-inner-right'>
           <span className='logged-in-user'>Hi User</span>
-          <Link to='/register' className='btn btn-sm btn-outline-danger ml-2'>
+          <button
+            onClick={() => navigate("/register")}
+            className='btn btn-outline-danger ml-2'
+          >
             Register
-          </Link>
-          <Link to='/login' className='btn btn-sm btn-outline-success ml-2'>
+          </button>
+          <button
+            onClick={() => navigate("/login")}
+            className='btn btn-outline-success ml-2'
+          >
             Login
-          </Link>
+          </button>
         </div>
       </nav>
     </div>
